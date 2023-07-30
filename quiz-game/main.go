8ff0e708bd66afbefc6940f52499d00ac353462c
Fryger <krzysztof@fryger.pl>
2023-07-30 12:00:26 +0200

    quiz game 2/2

---
+++ quiz-game/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var cvsPtr = flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer' (default 'problems.csv')")
+var limPtr = flag.Int("limit", 30, "the time limit for the quiz in seconds (default 30)")
+var scoreQ = 0
+var totalQ = 0
+
+func readCSV(file_name string) (data [][]string) {
+	f, err := os.Open(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, err = csvReader.ReadAll()
+
+	totalQ = len(data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+func read_quizes(quizes [][]string) {
+
+	var inp int
+
+	for i, d := range quizes {
+
+		fmt.Printf("Problem #%d: %s \n", i+1, d[0])
+		fmt.Scan(&inp)
+
+		ans, _ := strconv.Atoi(d[1])
+
+		if ans == inp {
+			scoreQ++
+		}
+	}
+
+}
+
+func time_quizes(t int) {
+
+	duration := time.Duration(t) * time.Second
+
+	time.AfterFunc(duration, func() {
+		summary_quiz()
+		os.Exit(0)
+	})
+
+}
+
+func summary_quiz() {
+	fmt.Printf("You scored %d out of %d", scoreQ, totalQ)
+
+}
+
+func main() {
+
+	flag.Parse()
+
+	quizes := readCSV(*cvsPtr)
+
+	fmt.Println("To start Quiz press Enter.")
+	fmt.Scanln()
+
+	time_quizes(*limPtr)
+	read_quizes(quizes)
+	summary_quiz()
+
+}
