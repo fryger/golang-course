5ab38ad1bd0ce2857750605dc955c2a5d4f11764
Fryger <krzysztof@fryger.pl>
2023-08-02 20:52:55 +0200

    final  CYOA

--- own-adventure/main.go
+++ own-adventure/main.go
@@ -5,6 +5,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
@@ -44,10 +46,34 @@ func readStory(filename string) (stories Story) {
 	return
 }
 
-var router *chi.Mux
+func parseStory(story Chapter, w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("templates/index.html")
+	catch(err)
+
+	err = t.Execute(w, story)
+	catch(err)
+}
 
 func main() {
 	stories := readStory(filename)
+	router := chi.NewRouter()
 
-	fmt.Print(stories)
+	for path, story := range stories {
+		// Create a new variable to hold the current story for this route
+		currentStory := story
+
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/intro", http.StatusFound)
+		})
+
+		router.Route(fmt.Sprintf("/%s", path), func(r chi.Router) {
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				parseStory(currentStory, w, r)
+			})
+		})
+	}
+
+	err := http.ListenAndServe(":8004", router)
+	catch(err)
 }
